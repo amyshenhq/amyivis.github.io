4f595218c1bcae54328f53dae03485a020c9ce12
amyshenhq <45400913+amyshenhq@users.noreply.github.com>
2019-03-04 15:50:34 +0100

    Update getvariable.js

--- insight-web/getvariable.js
+++ insight-web/getvariable.js
@@ -1,7 +1,7 @@
 function getQueryVariable(variable)
 {
        var query = window.location.search.substring(1);
-       var vars = query.split("");
+       var vars = query.split("&");
        for (var i=0;i<vars.length;i++) {
                var pair = vars[i].split("=");
                if(pair[0] == variable){return decodeURIComponent(pair[1]);}
